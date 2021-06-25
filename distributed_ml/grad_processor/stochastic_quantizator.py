Fixed random formula

distributed_ml/grad_processor/stochastic_quantizator.py
@@ -15,17 +15,21 @@ class StochasticQuantizator(GradientProcessor):
     def __process(self, flattened_layer):
         norm = flattened_layer.square().sum().sqrt()
         one_chunk_len = 2 / self.chunk_count
-        norm_layer = (flattened_layer / norm)
+        norm_component = (flattened_layer / norm)
+
+        chunk_count_to_component = ((norm_component + 1) / one_chunk_len).floor()  # from -1
+        component_left_bound = (chunk_count_to_component * one_chunk_len) - 1
 
         if self.random_bound:
-            # random (left or right) bound
-            left_or_right = torch.randint(2, size=flattened_layer.size()) * one_chunk_len
+            # random bound - [x, component, y]; y - x = one_chunk_len; prob for y = (component - x) / (y - x)
+            prob_for_right_bound = (norm_component - component_left_bound) / one_chunk_len
+            right_additional = (torch.rand(size=flattened_layer.size()) > prob_for_right_bound).int() * one_chunk_len
         else:
             # nearest bound
-            left_or_right = one_chunk_len / 2
+            right_additional = ((norm_component - component_left_bound) > one_chunk_len / 2).int() * one_chunk_len
 
-        chunk_count_to_component = ((norm_layer + left_or_right + 1) / one_chunk_len).floor()  # from -1
-        return ((chunk_count_to_component * one_chunk_len) - 1) * norm
+        # right_additional - 0 or one_chunk_len (left or right bound)
+        return (component_left_bound + right_additional) * norm
 
     def __do_per_layer(self, shard_grads: List[torch.Tensor]) -> List[torch.Tensor]:
         result = []